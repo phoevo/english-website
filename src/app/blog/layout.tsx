5ac86e739f161b2db8c0ff7114a6625ae1b03c96
Phoevo <148871904+phoevo@users.noreply.github.com>
2025-10-14 12:44:10 +0300

    blog updates

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,7 +1,6 @@
 import { DM_Sans, Geist } from "next/font/google";
 import Link from "next/link";
 import posts from "./posts";
-import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { CircleArrowLeft } from "lucide-react";
 
